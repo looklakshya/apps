5777229d181348810344d1e945f7c78f8ba8e10f
Lakshya Sharma <looklakshya@gmail.com>
2022-06-09 12:29:55 +0530

    My card - made finally

--- my_card/lib/main.dart
+++ my_card/lib/main.dart
@@ -14,50 +14,96 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            title: const Text("My Card"),
-            centerTitle: true,
-            backgroundColor: Colors.teal.shade900,
-          ),
-          backgroundColor: Colors.teal.shade700,
+          // appBar: AppBar(
+          //   title: const Text(
+          //     "Meet me!",
+          //     style: TextStyle(
+          //       fontFamily: "Alegreya",
+          //       fontSize: 25,
+          //     ),
+          //   ),
+          //   centerTitle: true,
+          //   backgroundColor: Colors.brown.shade900,
+          // ),
+          backgroundColor: Colors.brown.shade800,
           body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                color: Colors.red,
-                height: 79,
-                width: double.infinity,
+              const CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage("images/ls.jpg"),
               ),
-              Container(
-                color: Colors.orange,
-                height: 79,
-                width: double.infinity,
+              const Text(
+                "Lakshya Sharma",
+                style: TextStyle(
+                    fontFamily: "Pacifico",
+                    fontSize: 30,
+                    color: Colors.white,
+                    letterSpacing: 3),
               ),
-              Container(
-                color: Colors.yellow,
-                height: 79,
-                width: double.infinity,
+              const Text(
+                "Flutter Developer",
+                style: TextStyle(
+                    fontFamily: "Alegreya",
+                    fontSize: 20,
+                    color: Colors.white70,
+                    letterSpacing: 4),
               ),
-              Container(
-                color: Colors.green,
-                height: 79,
-                width: double.infinity,
+              const SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white60,
+                ),
               ),
-              Container(
-                color: Colors.blue,
-                height: 79,
-                width: double.infinity,
+              Card(
+                color: Colors.white,
+                // padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                margin: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.brown.shade700,
+                    size: 35.0,
+                  ),
+                  title: Text(
+                    "+91 77240 46986",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Sans",
+                      fontSize: 22,
+                      color: Colors.brown.shade700,
+                    ),
+                  ),
+                ),
               ),
-              Container(
-                color: Colors.indigo,
-                height: 79,
-                width: double.infinity,
+              Card(
+                color: Colors.white,
+                // padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email_rounded,
+                    color: Colors.brown.shade700,
+                    size: 35.0,
+                  ),
+                  title: Text(
+                    "looklakshya@gmail.com",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Sans",
+                      fontSize: 22,
+                      color: Colors.brown.shade700,
+                    ),
+                  ),
+                ),
               ),
-              Container(
-                color: Colors.purple,
-                height: 79,
-                width: double.infinity,
+              const SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white54,
+                ),
               ),
             ],
           ),
@@ -66,3 +112,43 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Row(children: [
+//                   Icon(
+//                     Icons.phone,
+//                     color: Colors.brown.shade700,
+//                     size: 35.0,
+//                   ),
+//                   SizedBox(
+//                     width: 15.0,
+//                   ),
+//                   Text(
+//                     "+91 77240 46986",
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                       fontFamily: "Sans",
+//                       fontSize: 22,
+//                       color: Colors.brown.shade700,
+//                     ),
+//                   ),
+//                 ]),
+
+// Row(children: [
+//                     Icon(
+//                       Icons.email_rounded,
+//                       color: Colors.brown.shade700,
+//                       size: 35.0,
+//                     ),
+//                     SizedBox(
+//                       width: 15.0,
+//                     ),
+//                     Text(
+//                       "looklakshya@gmail.com",
+//                       style: TextStyle(
+//                         fontWeight: FontWeight.bold,
+//                         fontFamily: "Sans",
+//                         fontSize: 22,
+//                         color: Colors.brown.shade700,
+//                       ),
+//                     ),
+//                   ]),
